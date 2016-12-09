da6f6ee1eed14db6ab7b5a8a5979c19a25faba5c
Matthews <MATTHR12@pfizer.com>
2016-12-09 15:55:59 -0500

    Indentation fix on function_tools.py

--- function_tools.py
+++ function_tools.py
@@ -4,24 +4,24 @@ def dict_map(f, d):
 	'''
 	Returns a new ditionary with key-value pairs (k, f(d[k])) where k is a key in d.
 	'''
-    return dict( (k, f(v)) for k,v in d.iteritems() )
+	return dict( (k, f(v)) for k,v in d.iteritems() )
 
 def tuple_map(fn, *iterables):
 	'''
 	Similar to map builtin, but returns a tuple instead of list.
 	'''
-    return tuple(map(fn, *iterables))
+	return tuple(map(fn, *iterables))
 
 def order_tuple(t, **sorted_kwargs):
 	'''
 	Similar to builtin sorted but returns a tuple.
 	'''
-    return tuple(sorted(t, **sorted_kwargs))
+	return tuple(sorted(t, **sorted_kwargs))
     
 def for_each(fn, *iterables):
 	'''
 	Similar to map builtin but assumes fn doesn't return anything useful and instead relies on side effects. Returns None.
 	'''
-    for args in izip(*iterables):
-        fn(*args)
+	for args in izip(*iterables):
+	    fn(*args)
 
