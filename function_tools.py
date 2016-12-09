cd830bad2477bcbf9c76272dbeb725f1cc1e1018
Matthews <MATTHR12@pfizer.com>
2016-12-09 15:53:18 -0500

    Added strings to function_tools.py

--- function_tools.py
+++ function_tools.py
@@ -1,15 +1,27 @@
 from itertools import izip
 
 def dict_map(f, d):
+	'''
+	Returns a new ditionary with key-value pairs (k, f(d[k])) where k is a key in d.
+	'''
     return dict( (k, f(v)) for k,v in d.iteritems() )
 
-def tuple_map(fn, iterable):
-    return tuple(map(fn, iterable))
+def tuple_map(fn, *iterables):
+	'''
+	Similar to map builtin, but returns a tuple instead of list.
+	'''
+    return tuple(map(fn, *iterables))
 
 def order_tuple(t, **sorted_kwargs):
+	'''
+	Similar to builtin sorted but returns a tuple.
+	'''
     return tuple(sorted(t, **sorted_kwargs))
     
 def for_each(fn, *iterables):
+	'''
+	Similar to map builtin but assumes fn doesn't return anything useful and instead relies on side effects. Returns None.
+	'''
     for args in izip(*iterables):
         fn(*args)
 
